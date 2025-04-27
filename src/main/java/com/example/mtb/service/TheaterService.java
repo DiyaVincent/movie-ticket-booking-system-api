f32cbc7c63098e67f7ddeaae5e0c7db6fe0a8713
DiyaVincent <diyavincent2001@gmail.com>
2025-04-27 21:25:55 +0530

    theater-creater

---
+++ src/main/java/com/example/mtb/service/TheaterService.java
@@ -0,0 +1,11 @@
+package com.example.mtb.service;
+
+import com.example.mtb.dto.TheaterRegisterationRequest;
+import com.example.mtb.dto.TheaterResponse;
+
+
+public interface TheaterService {
+
+    TheaterResponse addTheater(String email, TheaterRegisterationRequest theaterRegisterationRequest);
+
+}
